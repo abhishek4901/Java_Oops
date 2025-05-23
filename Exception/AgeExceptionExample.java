8da3d2c26d99aa9427fbddc7700e42e89a2f3ee3
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-05-23 06:24:12 +0530

    Create AgeExceptionExample.java

---
+++ Exception/AgeExceptionExample.java
@@ -0,0 +1,28 @@
+class AgeException extends Exception {
+
+    public AgeException(String message) {
+        super(message);
+    }
+}
+
+class Test {
+
+    void checkEligibility(int age) throws AgeException {
+        if (age < 18) {
+            throw new AgeException("Age is less than 18");
+        } else {
+            System.out.println("You are eligible to code.");
+        }
+    }
+}
+
+class AgeExceptionExample {
+    public static void main(String[] args) {
+        Test test = new Test();
+        try {
+            test.checkEligibility(165); // Example: Passing age less than 18 and pass above 18 then no exception
+        } catch (AgeException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
