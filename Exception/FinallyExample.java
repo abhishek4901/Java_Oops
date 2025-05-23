60da97969d656286171ce43dc4476f3b3970ff6b
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-05-23 06:25:51 +0530

    Create FinallyExample.java

---
+++ Exception/FinallyExample.java
@@ -0,0 +1,21 @@
+public class FinallyExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        try {
+            if (b == 0) {
+                throw new ArithmeticException("Division by zero is not allowed.");
+            }
+            int c = a / b;
+        } 
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index error: " + e);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Arithmetic error: " + e.getMessage());
+        }
+        finally {
+            System.out.println("Execution completed.");
+        }
+    }
+}
