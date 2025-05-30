af828c82add9cd89942ab4512c3e23715d124e8c
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-05-30 06:39:06 +0530

    Create EvenOddUsingString.java

---
+++ JavaBasic/EvenOddUsingString.java
@@ -0,0 +1,8 @@
+class EvenOddUsingString{
+    public static void main(String[] args) {
+        
+        int n = 90;
+        String [] result = {"Even","Odd"};
+        System.out.println(result[n%2]);
+    }
+}
