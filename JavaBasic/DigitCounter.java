48b22984532a796cf152a71349765bb76415233c
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-05-30 06:38:39 +0530

    Create DigitCounter.java

---
+++ JavaBasic/DigitCounter.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class DigitCounter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = sc.nextInt();
+
+        if (n == 0) {
+            System.out.println("Number of digits: 1");
+        } else if (n > 0) {
+            int digits = (int) Math.log10(n) + 1;
+            System.out.println("Number of digits: " + digits);
+        } 
+        sc.close();
+    }
+}
