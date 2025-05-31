b65e2bcf2db2474331651315db083ee6b32a8dd5
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-05-31 06:13:50 +0530

    Create DaemonThreadExamples.java

---
+++ Thread/DaemonThreadExamples.java
@@ -0,0 +1,36 @@
+
+
+
+class dthread extends Thread{
+    public void run (){ 
+         for (int i = 0; i < 10; i++) {
+        
+        if(Thread.currentThread().isDaemon()){
+            System.out.println(" daemon Thread");
+        }
+        else {
+            System.out.println(" this is not daemon Thread");
+            
+        }
+         }
+    }
+}
+
+
+public class DaemonThreadExamples {
+    public static void main(String[] args) {
+        dthread d1 = new dthread();
+        dthread d2 = new dthread();
+        dthread d3 = new dthread();
+        dthread d4 = new dthread();
+        d1.setDaemon(true);
+        
+        d1.start();
+        d2.setDaemon(true);
+        d2.start();//daemon thread have low priority then d2 is not excute  after  using d3 then 
+        d3.start();
+
+    }
+    
+}
+ 
