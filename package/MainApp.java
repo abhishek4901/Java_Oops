715fc029b9f8048f8107555f770f2081e520d6f9
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-06-01 16:40:39 +0530

    Package

---
+++ package/MainApp.java
@@ -0,0 +1,8 @@
+import mypack.Hello;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Hello h = new Hello();
+        h.sayHello();
+    }
+}
---
+++ package/mypack/Hello.java
@@ -0,0 +1,8 @@
+// File: lib/mypack/Hello.java
+package mypack;
+
+public class Hello {
+    public void sayHello() {
+        System.out.println("Hello from mypack!");
+    }
+}
