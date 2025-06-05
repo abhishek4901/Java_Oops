859c72eefa725d0c5ec997b05893fc1b6ab19944
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-06-05 06:27:04 +0530

    genricClassExample.java

--- Genric class/genricClassExample.java
+++ Genric class/genricClassExample.java
@@ -1,4 +1,4 @@
- class example<T>{
+ class Box<T>{
     private T value;
       T getValue(){//getter method
         return value;
@@ -13,7 +13,7 @@
 public class genricClassExample {
     public static void main(String[] args) {
       
-        example<Integer> exInt = new example<>(); // example with Integer type
+       Box<Integer> exInt = new BOx<>(); // example with Integer type
         exInt.setvalue(100);
         System.out.println(exInt.getValue()); // prints 100
 
@@ -22,7 +22,8 @@ public class genricClassExample {
         System.out.println(exStr.getValue()); // prints abhi
         
         // If you want to use both String and Integer, you can use a generic class with a Pair or similar structure.
-        // For now, example<T> only supports one type parameter.
+        // For now, example<T> only
+     supports one type parameter.
         // Here's an example using String:
         example<String> exStr2 = new example<>();
         exStr2.setvalue("another string");
