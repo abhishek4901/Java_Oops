c2a40b37cd092db21187aa6255a1703a24261e89
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-06-06 17:54:53 +0530

    Create GenricClass.java

---
+++ Genric class/GenricClass.java
@@ -0,0 +1,31 @@
+ class Box<T>{// T is type of parameter
+    private T value;
+      T getValue(){//getter method
+        return value;
+    }
+    public void setvalue(T value){//setter method
+        this.value = value;
+    }
+
+}
+public class GenricClass {
+    public static void main(String[] args) {
+
+       Box<Integer> exInt = new Box<>(); // example with Integer type
+        exInt.setvalue(100);
+        System.out.println(exInt.getValue()); // prints 100
+
+        Box<String> exStr = new Box<>(); // example with String type
+        exStr.setvalue("abhi");
+        System.out.println(exStr.getValue()); // prints abhi
+        
+        // If you want to use both String and Integer, you can use a generic class with a Pair or similar structure.
+        // For now, example<T> only
+     //supports one type parameter
+        // Here's an example using String
+        Box<String> exStr2 = new Box<>();
+        exStr2.setvalue("another string");
+        System.out.println(exStr2.getValue()); // prints another string
+    }
+    
+}
