8a0a53848a57eb010d4faaa4c5f303aec2990900
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-06-12 08:29:10 +0530

    Create MultipleInheritancceExample.java

---
+++ Inheritance/MultipleInheritancceExample.java
@@ -0,0 +1,18 @@
+class dog {
+    void bark(){
+        System.err.println("Dog barks");
+    }
+}
+class Cat {
+    void mews(){
+        System.err.println("Cat Mews");
+
+    }
+}
+
+
+
+
+public class MultipleInheritancceExample {
+    
+}
