5089e635fc651ce021d4d261906e23090bda723b
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-06-19 18:12:03 +0530

    MultipleException.java

--- Exception/MultipleException.java
+++ Exception/MultipleException.java
@@ -1,7 +1,7 @@
 import java.util.Scanner; // Import Scanner for input
 
 public class MultipleException {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in); // Create Scanner object for input
   
         try { 
